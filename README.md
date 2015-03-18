ee65d1d8199f6e4966ec04ccccb52a6b91ac2eae
Henrik Ekblad <henrik.ekblad@gmail.com>
2015-03-18 10:18:20 +0100

    First release

--- README.md
+++ README.md
@@ -1,27 +1,8 @@
 # NodeBB: Buttons Galore
 
-This plugin aims to add several buttons to the NodeBB composer in the future. As for now, I'm still trying to learn how to accomplish this without breaking everything (which this plugin has, a few times). This plugin is designed for those using Github flavored Markdown (why wouldn't you be?). As a result, make sure that your NodeBB install is using this Markdown flavor.
+Simply adds a code button to composer. This plugin will be obsolete once TedRinehart add configuration support to https://github.com/TedRinehart/nodebb-plugin-buttons-galore 
 
-
-## Accomplished
-* Underline (esiao)
-* Alignment of content (esiao)
-* ~~Strikethrough effect~~
-
-```
-  Code tags
-  ```
-  
-> Easy Quote button for websites or a user
-  
-
-## Future? 
-* Change order of buttons in composer / mesh with default ones - _This way Underline and strikethrough could be closer to Bold and Italics_
-* Allow admins to disable specific buttons
-
-### Regarding Colors and Spoilers
-There are already plugins for NodeBB that allow the use of spoilers and colors in posts, so I won't be incorporating them into this plugin. As a side note, this entire project is already a fork from psychobunny's spoilers plugin. I don't want to step on the toes of these plugin authors.
 
 ## Installation
 
-npm install nodebb-plugin-buttons-galore
+npm install nodebb-plugin-code-button
--- buttons.js
+++ buttons.js
@@ -1,23 +1,5 @@
 $('document').ready(function() {
 	require(['composer', 'composer/controls'], function(composer, controls) {
-		composer.addButton('fa fa-strikethrough', function(textarea, selectionStart, selectionEnd) {
-			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, '~~Insert Text Here~~');
-				controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 18);
-			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '~~','~~');
-				controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 2);
-			}
-		});
-		composer.addButton('fa fa-underline', function(textarea, selectionStart, selectionEnd) {
-			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, '~Insert Text Here~');
-				controls.updateTextareaSelection(textarea, selectionStart + 1, selectionEnd + 17);
-			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '~','~');
-				controls.updateTextareaSelection(textarea, selectionStart + 1, selectionEnd + 2);
-			}
-		});	
 		composer.addButton('fa fa-code', function(textarea, selectionStart, selectionEnd) {
 			if(selectionStart === selectionEnd){
 				controls.insertIntoTextarea(textarea, '```\nInsert Code Here\n```');
@@ -25,50 +7,6 @@ $('document').ready(function() {
 			} else {
 				controls.wrapSelectionInTextareaWith(textarea, '```\n','\n```\n');
 				controls.updateTextareaSelection(textarea, selectionStart + 4, selectionEnd + 4);
-			}
-		});
-		composer.addButton('fa fa-quote-right', function(textarea, selectionStart, selectionEnd) {
-			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, '@Username / Website said:\n> ');
-				controls.updateTextareaSelection(textarea, selectionStart + 28, selectionEnd + 28);
-			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '@Username / Website said:\n> ','\n\n');
-			}
-		});
-		composer.addButton('fa fa-align-left', function(textarea, selectionStart, selectionEnd) {
-			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, '<-Insert Text Here<-');
-				controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 18);
-			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '<-','<-');
-				controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 2);
-			}
-		});
-		composer.addButton('fa fa-align-center', function(textarea, selectionStart, selectionEnd) {
-			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, '->Insert Text Here<-');
-				controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 18);
-			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '->','<-');
-				controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 2);
-			}
-		});
-		composer.addButton('fa fa-align-right', function(textarea, selectionStart, selectionEnd) {
-			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, '->Insert Text Here->');
-				controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 18);
-			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '->','->');
-				controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 2);
-			}
-		});
-		composer.addButton('fa fa-align-justify', function(textarea, selectionStart, selectionEnd) {
-			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, '=>Insert Text Here<=');
-				controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 18);
-			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '=>','<=');
-				controls.updateTextareaSelection(textarea, selectionStart + 2, selectionEnd + 2);
 			}
 		});
 	});
