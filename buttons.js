a11edce9d741bb31b17632180874a46a0b9ada42
Henrik Ekblad <henrik.ekblad@gmail.com>
2017-01-21 15:03:34 +0100

    Try again

--- buttons.js
+++ buttons.js
@@ -1,16 +1,18 @@
 //formatting.addButtonDispatch() is used here because 
 //composer.addButton() is missing the argument for the 
-//tooltip title.  This argument was added in composer version 3.0.27.  
-$('document').ready(function() {
-	require(['composer', 'composer/controls', 'composer/formatting'], function(composer, controls, formatting) {
-		formatting.addButtonDispatch('code', function(textarea, selectionStart, selectionEnd) {
-			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, '```\nInsert Code Here\n```');
-				controls.updateTextareaSelection(textarea, selectionStart + 4, selectionEnd + 21);
-			} else {
-				controls.wrapSelectionInTextareaWith(textarea, '```\n','\n```\n');
-				controls.updateTextareaSelection(textarea, selectionStart + 4, selectionEnd + 4);
-			}
+//tooltip title.  This argument was added in composer version 3.0.27.
+$(document).ready(function() {
+	$(window).on('action:composer.enhanced', function() {
+		require(['composer/controls', 'composer/formatting'], function(controls, formatting) {
+			formatting.addButtonDispatch('code-button', function(textarea, selectionStart, selectionEnd) {
+				if(selectionStart === selectionEnd){
+					controls.insertIntoTextarea(textarea, '```\nInsert Code Here\n```');
+					controls.updateTextareaSelection(textarea, selectionStart + 4, selectionEnd + 21);
+				} else {
+					controls.wrapSelectionInTextareaWith(textarea, '```\n','\n```\n');
+					controls.updateTextareaSelection(textarea, selectionStart + 4, selectionEnd + 4);
+				}
+			});
 		});
 	});
 });
--- library.js
+++ library.js
@@ -2,14 +2,9 @@
 
 var codeButtons = {};
 
-codeButtons.registerFormatting = function(data, callback) {
-	var formatting = [
-		{name: 'code', className: 'fa fa-code', title: '[[codeButtons:code]]'},
-	];
-
-	data.options = data.options.concat(formatting);
-
-	callback(null, data);	
-}
+codeButtons.addComposerButton = function(payload, callback) {
+	payload.options.push({ name: 'code-button', className: 'fa fa-code', title: "Insert Code Block"});
+	callback(null, payload);
+};
 
 module.exports = codeButtons;
