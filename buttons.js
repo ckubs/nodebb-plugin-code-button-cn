75b9fa08bed658e61b50821b26f8cdd7fb4283fc
王开智 <kidswong999@163.com>
2018-06-12 15:10:54 +0800

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -5,7 +5,7 @@ $('document').ready(function() {
 	require(['composer/controls', 'composer/formatting'], function(controls, formatting) {
 		formatting.addButtonDispatch('code-button', function(textarea, selectionStart, selectionEnd) {
 			if(selectionStart === selectionEnd){
-				controls.insertIntoTextarea(textarea, '\n```\n请在这里粘贴代码\n```');
+				controls.insertIntoTextarea(textarea, '\n```\n请在这里粘贴代码\n```\n');
 				controls.updateTextareaSelection(textarea, selectionStart + 5, selectionEnd + 13);
 			} else {
 				controls.wrapSelectionInTextareaWith(textarea, '\n```\n','\n```\n');
