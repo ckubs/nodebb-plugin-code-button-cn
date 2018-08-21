a1e7db7020f5febbe02128b22ec5224fa4531c65
Gaein_nidb <36162655+nidbCN@users.noreply.github.com>
2018-08-21 21:16:34 +0800

    Update library.js
    按钮中文

--- library.js
+++ library.js
@@ -3,7 +3,7 @@
 var codeButtons = {};
 
 codeButtons.addComposerButton = function(payload, callback) {
-	payload.options.push({ name: 'code-button', className: 'fa fa-code', title: "Insert Code Block"});
+	payload.options.push({ name: 'code-button', className: 'fa fa-code', title: "添加代码"});
 	callback(null, payload);
 };
 
